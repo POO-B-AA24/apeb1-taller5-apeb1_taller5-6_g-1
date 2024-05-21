c9a4fa13ae52630b09409cedce886e8a22e59cfc
Luis Aguilar <149074526+LuisAg1256@users.noreply.github.com>
2024-05-20 21:17:22 -0500

    Problema-3

---
+++ Solucion_Codigo/Taller5/src/Problema3_Departamento.java
@@ -0,0 +1,32 @@
+
+public class Problema3_Departamento {
+    private String nombre;
+    private int numeroEmpleados;
+    private double produccionAnual;
+    private char categoria;
+
+    public Problema3_Departamento(String nombre, int numeroEmpleados, double produccionAnual) {
+        this.nombre = nombre;
+        this.numeroEmpleados = numeroEmpleados;
+        this.produccionAnual = produccionAnual;
+        this.categoria = determinarCategoria();
+    }
+
+    private char determinarCategoria() {
+        if (numeroEmpleados > 20 && produccionAnual > 1000000) {
+            return 'A';
+        } else if (numeroEmpleados == 20 && produccionAnual == 1000000) {
+            return 'B';
+        } else if (numeroEmpleados == 10 && produccionAnual == 500000) {
+            return 'C';
+        } else {
+            return 'N';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Departamento: %s, Empleados: %d, Producción Anual: %.2f, Categoría: %c",
+                nombre, numeroEmpleados, produccionAnual, categoria);
+    }
+}
